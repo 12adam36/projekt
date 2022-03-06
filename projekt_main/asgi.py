641e40071ee8e242e9c1002494ac31259bdd8f26
12adam36 <56118411+12adam36@users.noreply.github.com>
2022-03-06 22:43:04 +0100

    3

--- projekt_main/asgi.py
+++ projekt_main/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for projekt project.
+ASGI config for projekt_main project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projekt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projekt_main.settings')
 
 application = get_asgi_application()

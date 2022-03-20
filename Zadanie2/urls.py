fb3eded9f6896d26f3e338fd0ad7c2ec11ea0f0b
12adam36 <56118411+12adam36@users.noreply.github.com>
2022-03-20 18:27:59 +0100

    z3_endpoint2_4

--- Zadanie2/urls.py
+++ Zadanie2/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name=''),
     path('patches', views.patches, name=''),
-    path('players/<int:input_player_id>/game_exp', views.game_exp, name=''),
+    path('players/<int:input_player_id>/game_exp/', views.game_exp, name=''),
     path('players/<int:input_player_id>/game_objectives', views.game_exp, name=''),
     path('players/<int:input_player_id>/abilities', views.game_exp, name=''),
 ]

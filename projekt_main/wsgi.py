1b8dcd9f8ba5421d8e502789b452a6ff4143bc20
12adam36 <56118411+12adam36@users.noreply.github.com>
2022-04-20 14:09:33 +0200

    wsgi update sys

--- projekt_main/wsgi.py
+++ projekt_main/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projekt_main.settings')
+path = os.path.abspath(os.path.join(__file__, '..', '..'))
+
+if path not in sys.path:
+    sys.path.append(path)
 
 application = get_wsgi_application()
